a7920528b8d5b406f1d8cdf1028e48ef5c73d5b2
LironeFitoussi <lironefit@icloud.com>
2024-01-28 09:07:46 +0200

    Progress in States

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import ProjectsSidebar from "./components/ProjectsSidebar/ProjectsSidebar";
 import NewProject from "./components/NewProject/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject () {
+    setProjectsState(prevProject => {
+      return {
+        ...prevProject,
+        selectedProjectId: null,
+      };
+    })
+  }
+
+  let cotent;
+  if ()
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <NoProjectSelected onStartAddProject={handleStartAddProject} />
     </main>
   );
 }
