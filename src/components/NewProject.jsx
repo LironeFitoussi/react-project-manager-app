66a793ecbc2a02d2bf04088a9326dc056a261502
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-20 22:14:22 +0200

    Lesson progression

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,40 @@
+import { useRef } from 'react';
 import Input from './Input.jsx';
 
-const NewProject = () => {
+const NewProject = ({onAdd}) => {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value
+        const enteredDescription = description.current.value
+        const enteredDueDate = dueDate.current.value
+
+        // Validation ...
+        if (enteredTitle.length === 0) {
+            alert('Please enter a title')
+            return
+        }
+
+        if (enteredDescription.length === 0) {
+            alert('Please enter a description')
+            return
+        }
+
+        if (enteredDueDate.length === 0) {
+            alert('Please enter a due date')
+            return
+        }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate,
+        })
+
+    }
+
     return (
         <div className='w-[35rem] mt-16'>
             <menu className='flex items-center justify-end gap-4 my-4'>
@@ -8,13 +42,16 @@ const NewProject = () => {
                     <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
                 </li>
                 <li>
-                    <button className='bg-stone-800 py-2 px-6 rounded-md text-stone-50 hover:bg-stone-950'>Save</button>
+                    <button 
+                        className='bg-stone-800 py-2 px-6 rounded-md text-stone-50 hover:bg-stone-950'
+                        onClick={handleSave}
+                    >Save</button>
                 </li>
             </menu>
             <div>
-                <Input label='Title' />
-                <Input label='Description' textarea />
-                <Input label='Due Date' />
+                <Input ref={title} label='Title' />
+                <Input ref={description} label='Description' textarea />
+                <Input ref={dueDate} label='Due Date' />
             </div>
         </div>
     )
